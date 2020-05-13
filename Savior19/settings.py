fc0798b2c8cc6ecc4063267045aa85308506683d
aadhityasw <aadhityas@gmail.com>
2020-05-13 17:09:04 +0530

    Update settings.py

--- Savior19/settings.py
+++ Savior19/settings.py
@@ -19,10 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-
-# Deployment options :
 # Change this to True to make all the necessary changes to all the settings and to deploy.
-DEPLOY = True
+DEPLOY = False
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mi)vammyq)bct_3ga2#pqtaa7rx($=6$=5p7yd1)##mg26#rjs'
@@ -33,16 +32,12 @@ if DEPLOY == True :
 else :
     DEBUG = True
 
-
-if DEPLOY == True :
-    ALLOWED_HOSTS = ['savior19-staging.herokuapp.com']
-else :
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = ['savior19-staging.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,12 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-if DEPLOY == True :
-    INSTALLED_APPS.insert(0, 'whitenoise.runserver_nostatic')
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -63,9 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if DEPLOY == True :
-    # This should be just below the 'django.middleware.security.SecurityMiddleware'.
-    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 ROOT_URLCONF = 'Savior19.urls'
 
@@ -91,7 +81,7 @@ WSGI_APPLICATION = 'Savior19.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+# Use the local sqlite database.
 if DEPLOY == True :
     # Use the postgre database on heroku.
     DATABASES = {
@@ -105,7 +95,6 @@ if DEPLOY == True :
         }
     }
 else :
-    # Use the local sqlite database.
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -151,7 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-if DEPLOY == True :
-    # To use the whitenoise to handle the static files (as heroku dosen't do this on its own).
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# To use the whitenoise to handle the static files (as heroku dosen't do this on its own).
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
